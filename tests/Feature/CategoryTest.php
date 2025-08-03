61219fbae3629d7a724588a4346b167fc1cecebb
Haiiyusss <128706837+dhayussyahri@users.noreply.github.com>
2025-08-04 00:47:17 +0700

    Belajar Insert Many dan membuat log info

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -17,6 +17,25 @@ class CategoryTest extends TestCase
         $result = $category->save();
 
         self::assertTrue($result);
+    }
 
+    public function testInsertMany()
+    {
+        $categories = [];
+        for($i = 0; $i<10; $i++){
+            $categories[] = [
+                "id" => "ID $i",
+                "name" => "Name $i"
+            ];
+        }
+
+        // $result = Category::query()->insert($categories);
+        $result = Category::insert($categories);
+        self::assertTrue($result);
+
+        // $total = Category::query()->count();
+        $total = Category::count();
+
+        self::assertEquals(10, $total);
     }
 }
