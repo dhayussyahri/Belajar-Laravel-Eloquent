0f08b8ff0b9ea1aa8642e6eb68afe0d9b835ef20
Haiiyusss <128706837+dhayussyahri@users.noreply.github.com>
2025-08-11 16:44:30 +0700

    Belajar Membuat Accessors dan Mutators dengan unit test

---
+++ app/Models/Person.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use PhpParser\Node\Expr\Cast\Array_;
+
+class Person extends Model
+{
+    protected $table = "persons";
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    public $incrementing = true;
+    public $timestamps = true;
+
+    protected function fullName() : Attribute
+    {
+        return Attribute::make(
+            get: function():string {
+                return $this->first_name . ' ' . $this->last_name;
+            },
+            set: function(string $value) :Array
+            {
+                $names = explode(" ", $value);
+                return [
+                    'first_name' => $names[0],
+                    'last_name' => $names[1] ?? ''
+                ];
+            }
+        );
+    }
+}
---
+++ tests/Feature/PersonTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Person;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PersonTest extends TestCase
+{
+    public function testPerson()
+    {
+        $person = new Person();
+        $person->first_name = "Dhayus";
+        $person->last_name = "Syahri";
+        $person->save();
+
+        self::assertEquals("Dhayus Syahri", $person->fullName);
+
+        $person->fullName = "Joko Morro";
+        $person->save();
+
+        self::assertEquals("Joko", $person->first_name);
+        self::assertEquals("Morro", $person->last_name);
+
+    }
+}
